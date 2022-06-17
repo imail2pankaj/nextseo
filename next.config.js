c7a46da88a14ebfc52ded3f0e0ccf8a26ffb087f
pankaj <imail2pankaj@gmail.com>
2022-06-17 17:18:14 +0530

    Next seo plugin example

---
+++ next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          // options: pluginOptions.options,
+        },
+      ],
+    })
+
+    return config
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
+}
