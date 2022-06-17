5bfcca46319328ad4b4ce92fe01d60a406d34689
pankaj <imail2pankaj@gmail.com>
2022-06-17 17:26:32 +0530

    Opengraph nextseo

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,30 @@ export default function Home() {
         <NextSeo
           title={course.seo_title}
           description={course.search_description}
+          openGraph={{
+            url: 'https://www.url.ie/a',
+            title: course.seo_title,
+            description: course.search_description,
+            images: [
+              {
+                url: 'https://www.example.ie/og-image-01.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Og Image Alt',
+                type: 'image/jpeg',
+              },
+              {
+                url: 'https://www.example.ie/og-image-02.jpg',
+                width: 900,
+                height: 800,
+                alt: 'Og Image Alt Second',
+                type: 'image/jpeg',
+              },
+              { url: 'https://www.example.ie/og-image-03.jpg' },
+              { url: 'https://www.example.ie/og-image-04.jpg' },
+            ],
+            site_name: 'SiteName',
+          }}
         />
         {/* <Head>
           <title>Create Next App</title>
